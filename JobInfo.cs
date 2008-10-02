b24e1534c571b6a2fa4936e2468c9b7d0d015aa3
MaciekTalaska <maciek.talaska@gmail.com>
2008-10-02 22:58:36 +0000

    NEW: + dialog for displaying info about selected job (activated by double-click) + app icon + wrapper for ListView control - avoiding flickering while refreshing job's list

---
+++ JobInfo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BitsMonitor
+{
+	public partial class JobInfo : Form
+	{
+		private bool _readOnly;
+		public bool ReadOnly
+		{
+			get { return _readOnly; }
+			set { _readOnly = value; }
+		}
+
+		public string JobName
+		{
+			get { return this.txtJobName.Text; }
+			set
+			{
+				if (!string.IsNullOrEmpty(value))
+					this.txtJobName.Text = value;
+			}
+		}
+
+		public string JobUrl
+		{
+			get { return this.txtJobUrl.Text; }
+			set
+			{
+				if (!string.IsNullOrEmpty(value))
+					this.txtJobUrl.Text = value;
+			}
+		}
+
+		public JobInfo()
+		{
+			InitializeComponent();
+		}
+
+		protected void ReadOnlyChanged( )
+		{
+			foreach (Control c in this.Controls)
+			{
+				c.Enabled = _readOnly;
+			}
+		}
+
+		private void btn_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
