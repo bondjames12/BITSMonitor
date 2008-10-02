3ed5600a35a28b2dd77186787ab3739295ce93b7
MaciekTalaska <maciek.talaska@gmail.com>
2008-10-02 22:58:38 +0000

    NEW: + dialog for displaying info about selected job (activated by double-click) + app icon + wrapper for ListView control - avoiding flickering while refreshing job's list

---
+++ Controls/mListView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BitsMonitor
+{
+	public partial class mListView : ListView
+	{
+		public mListView()
+		{
+			InitializeComponent();
+			this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+			this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+		}
+	}
+}
