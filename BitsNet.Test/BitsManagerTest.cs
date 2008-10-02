46b57f6fd77db2a76b3fbf4077b9c7329a00c923
MaciekTalaska <maciek.talaska@gmail.com>
2008-10-02 22:56:10 +0000

    NEW: + simple unit test (just a scratch) - just to make sure mbUnit works properly

--- BitsNet.Test/BitsManagerTest.cs
+++ BitsNet.Test/BitsManagerTest.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using BitsNet;
+using MbUnit.Framework;
 
 namespace BitsNet.Test
 {
+	[TestFixture]
 	public class BitsManagerTest
 	{
+		[Row("url","jobName","directory")]
+		[RowTest]
+		public void AddJob(string url, string jobName, string directory)
+		{
+			BitsManager.AddJob(url, jobName, directory);
+		}
 	}
 }
