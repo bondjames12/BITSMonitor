d348923d59cc60beb3dae703567b2452f825e6b0
MaciekTalaska <maciek.talaska@gmail.com>
2008-10-13 21:07:04 +0000

    NEW: + function for extracting (or at least trying to extract) filename from URL with some GET parameters, added

---
+++ Utility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BitsMonitor
+{
+	class Utility
+	{
+		public static string GetFileNameFromUrlWithGet(string fileName)
+		{
+			fileName = fileName.Substring(fileName.IndexOf("?"), fileName.Length - fileName.IndexOf("?"));
+			Regex exp = new Regex(@"\?.*=");
+			if (exp.IsMatch(fileName))
+			{
+				MatchCollection mc = exp.Matches(fileName);
+				for (int i = mc.Count - 1; i >= 0; i--)
+				{
+					fileName = fileName.Remove(mc[i].Index, mc[i].Length);
+				}
+			}
+			return "";
+		}
+
+	}
+}
