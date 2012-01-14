91dd414d03349fd83655d9dc1c784e96f5c44d93
MaciekTalaska <maciek.talaska@gmail.com>
2012-01-14 18:27:39 +0000

    FIX: application icon is shown on all dialogs

--- ExceptionInfo.Designer.cs
+++ ExceptionInfo.Designer.cs
@@ -28,62 +28,63 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.btnDone = new System.Windows.Forms.Button();
-			this.txtExceptionInfo = new System.Windows.Forms.TextBox();
-			this.txtInfo = new System.Windows.Forms.TextBox();
-			this.SuspendLayout();
-			// 
-			// btnDone
-			// 
-			this.btnDone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.btnDone.Location = new System.Drawing.Point(297, 231);
-			this.btnDone.Name = "btnDone";
-			this.btnDone.Size = new System.Drawing.Size(75, 23);
-			this.btnDone.TabIndex = 0;
-			this.btnDone.Text = "Done";
-			this.btnDone.UseVisualStyleBackColor = true;
-			this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
-			// 
-			// txtExceptionInfo
-			// 
-			this.txtExceptionInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-						| System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.txtExceptionInfo.Location = new System.Drawing.Point(12, 104);
-			this.txtExceptionInfo.Multiline = true;
-			this.txtExceptionInfo.Name = "txtExceptionInfo";
-			this.txtExceptionInfo.Size = new System.Drawing.Size(360, 121);
-			this.txtExceptionInfo.TabIndex = 2;
-			// 
-			// txtInfo
-			// 
-			this.txtInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.txtInfo.Location = new System.Drawing.Point(12, 12);
-			this.txtInfo.Multiline = true;
-			this.txtInfo.Name = "txtInfo";
-			this.txtInfo.ReadOnly = true;
-			this.txtInfo.Size = new System.Drawing.Size(360, 86);
-			this.txtInfo.TabIndex = 3;
-			this.txtInfo.Text = "Exception occured. Please provide additional info about the actions performed bef" +
-				"ore. Log files (.log) are stored in application folder. Please send log files to" +
-				" the author of this app. \r\n\r\nThank you.";
-			// 
-			// ExceptionInfo
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(384, 266);
-			this.Controls.Add(this.txtExceptionInfo);
-			this.Controls.Add(this.btnDone);
-			this.Controls.Add(this.txtInfo);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
-			this.MinimumSize = new System.Drawing.Size(400, 300);
-			this.Name = "ExceptionInfo";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "ExceptionInfo";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExceptionInfo));
+            this.btnDone = new System.Windows.Forms.Button();
+            this.txtExceptionInfo = new System.Windows.Forms.TextBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDone
+            // 
+            this.btnDone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDone.Location = new System.Drawing.Point(297, 231);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(75, 23);
+            this.btnDone.TabIndex = 0;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // txtExceptionInfo
+            // 
+            this.txtExceptionInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtExceptionInfo.Location = new System.Drawing.Point(12, 104);
+            this.txtExceptionInfo.Multiline = true;
+            this.txtExceptionInfo.Name = "txtExceptionInfo";
+            this.txtExceptionInfo.Size = new System.Drawing.Size(360, 121);
+            this.txtExceptionInfo.TabIndex = 2;
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInfo.Location = new System.Drawing.Point(12, 12);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.Size = new System.Drawing.Size(360, 86);
+            this.txtInfo.TabIndex = 3;
+            this.txtInfo.Text = "Exception occured. Please provide additional info about the actions performed bef" +
+    "ore. Log files (.log) are stored in application folder. Please send log files to" +
+    " the author of this app. \r\n\r\nThank you.";
+            // 
+            // ExceptionInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 266);
+            this.Controls.Add(this.txtExceptionInfo);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.txtInfo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "ExceptionInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ExceptionInfo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
